79e63356c55c1ff434c11a9abb2c7c0b72a3de7e
Raghavendra Iyer <raghavendra.iyer@qpidhealth.com>
2017-05-23 12:02:54 -0400

    remove shelljs dependency

--- config/environment.js
+++ config/environment.js
@@ -1,21 +1,8 @@
 /* eslint-env node */
 'use strict';
 
-var gitRepoInfo = require('git-repo-info');
-require('shelljs/global');
-
-var dateFromGit = function() {
-  return exec('git log -1 --format="%ad" HEAD', { silent: true }).output.trim();
-};
-
 module.exports = function(/* environment, appConfig */) {
-  var info = gitRepoInfo();
-
-  // if we weren't able to parse a date from the commit, ask git
-  info.date = info.date || dateFromGit();
-
   return {
-    gitInfo: info,
     appVersion: process.env.APP_VERSION
   };
 };
